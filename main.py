4e5052b9b1269eec100e905b619048efde96c92c
ParkerCircus <152338567+ParkerCircus@users.noreply.github.com>
2024-02-26 22:49:58 -0500

    lower added to payment

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ subtotal = 20
 def payment_type():
     payment = input("Select a payment type: cash, credit, check ")
     while True:
-        if payment == "cash":
+        if payment.lower() == "cash":
             tendered_amount = float(input("Enter amount of cash: "))
             print(f"Your change is {tendered_amount - subtotal}")
             break
-        elif payment == "check":
+        elif payment.lower() == "check":
             check_number = input("Please enter your check number: ")
             break
-        elif payment == "credit":
+        elif payment.lower() == "credit":
             credit_card_number = input("Please enter your credit card number: ")
             expiration = input("Please enter your card's expiration date (mm/yy): ")
-            CVV = input("Please enter your card's CVV number: ")
+            cvv = input("Please enter your card's CVV number: ")
             break
         else:
             payment = input("Invalid response. Please enter: 'cash', 'credit', or 'check'")
