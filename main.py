630b915b7b74bc91a3b9b5cdb05916b3290ecf3f
Shenedia Ellington <artistic.nita@gmail.com>
2024-02-28 21:42:07 -0500

    created alternative list for product class

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-shoes = [{"name": "Air Jordan 1", "category": "Sneaker", "description": "BK and WH High Top", "price": 125},
-         {"name": "Vans", "category": "Sneaker", "description": "BK and WH Sk8 High Top", "price": 65},
-         {"name": "Converse", "category": "Sneaker", "description": "BK and WH Chuck Taylor Low Top", "price": 60},
-         {"name": "Becca Flat", "category": "Dress Shoe", "description": "BK Pointed Toe Slingback Flat", "price": 120},
-         {"name": "Cole Haan ", "category": "Dress Shoe", "description": "BR Classic Penny Loafer", "price": 158},
-         {"name": "Gucci", "category": "Dress Shoe", "description": "BK Leather Lace-Up with Double G", "price": 890},
-         {"name": "Birkenstock", "category": "Sandal", "description": "BK Suede Leather", "price": 145},
-         {"name": "Crocs", "category": "Sandal", "description": "Hello Kitty Stomp Slide", "price": 50},
-         {"name": "Olukai ", "category": "Sandal", "description": "Dark Java Ohana", "price": 75},
-         {"name": "Orthofeet", "category": "Indoor Shoe", "description": "BK Louise Stretch Knit", "price": 90},
-         {"name": "Diodora", "category": "Indoor Shoe", "description": "WH, OR, and TUR Futsal Boots", "price": 115},
-         {"name": "Cloud Slides", "category": "Indoor Shoe", "description": "YL Original", "price": 27}]
-
-
-class Product:
+class Shoe:
     def __init__(self, name, category, description, price):
         self.name = name
         self.category = category
         self.description = description
         self.price = price
+
+
+shoes = [Shoe("Air Jordan 1", "Sneaker", "BK and WH High Top", 125),
+         Shoe("Vans", "Sneaker", "BK and WH Sk8 High Top", 65),
+         Shoe("Converse", "Sneaker", "BK and WH Chuck Taylor Low Top", 60),
+         Shoe("Becca Flat", "Dress Shoe", "BK Pointed Toe Slingback Flat", 120),
+         Shoe("Cole Haan ", "Dress Shoe", "BR Classic Penny Loafer", 158),
+         Shoe("Gucci", "Dress Shoe", "BK Leather Lace-Up with Double G", 890),
+         Shoe("Birkenstock", "Sandal", "BK Suede Leather", 145),
+         Shoe("Crocs", "Sandal", "Hello Kitty Stomp Slide", 50),
+         Shoe("Olukai ", "Sandal", "Dark Java Ohana", 75),
+         Shoe("Orthofeet", "Indoor Shoe", "BK Louise Stretch Knit", 90),
+         Shoe("Diodora", "Indoor Shoe", "WH, OR, and TUR Futsal Boots", 115),
+         Shoe("Cloud Slides", "Indoor Shoe", "YL Original", 27)]
